7acf993d50be782aeba2a491a73db72b5bce050f
Iris Zhou <iris.zhou@lever.co>
2021-10-15 16:41:33 -0400

    remove stuff!

--- client.ts
+++ client.ts
@@ -3,12 +3,12 @@ const { Client } = require("@microsoft/microsoft-graph-client");
 const { TokenCredentialAuthenticationProvider } = require("@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials");
 const { AuthorizationCodeCredential, ClientSecretCredential, } = require("@azure/identity");
 
-const clientId = '4ecc15f0-3ba6-4333-b937-7235e3688945'; //client id of the registered application
+const clientId = 'go to Azure AD and find it'; //client id of the registered application
 const scopes: string[] = [
     'https://graph.microsoft.com/.default'
 ]
-const tenantId = '4a7fbee1-6041-40b1-a613-86577b4f827f';
-const clientSecret = '4l77Q~72d7QkWiCFsHfTZZDfb1D_TkFfZfv-a';
+const tenantId = 'go to Azure AD and find it';
+const clientSecret = 'go to Azure AD and find it';
 
 const credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
 const authProvider = new TokenCredentialAuthenticationProvider(credential,  {
